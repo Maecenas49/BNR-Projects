9441ea799b5eef018fd4342717a0419b003f629d
Maecenas49 <Mike_springer_49@hotmail.com>
2015-03-08 17:17:46 -0400

    Created Videoview to play video

--- HelloMoon/app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ HelloMoon/app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -1,22 +1,32 @@
 package com.bignerdranch.android.hellomoon;
 
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 /**
  * Created by Mike on 3/5/2015.
  */
 public class HelloMoonFragment  extends Fragment{
+    private final String TAG = "HelloMoonFragment";
+
     private Button mPlayButton;
     private Button mStopButton;
     private Button mPauseButton;
 
     private AudioPlayer mPlayer = new AudioPlayer();
+
+    private VideoView mVideoView;
+    private MediaController mMediaController;
 
 
     @Override
@@ -47,6 +57,16 @@ public class HelloMoonFragment  extends Fragment{
             }
         });
 
+        mVideoView = (VideoView) v.findViewById(R.id.hellomoon_VideoView);
+
+        mMediaController = new MediaController(getActivity());
+        mMediaController.setAnchorView(mVideoView);
+        mVideoView.setMediaController(mMediaController);
+        Log.d(TAG, "android.resource://" +
+               "com.bignerdranch.android.hellomoon/raw/apollo_17_stroll");
+        mVideoView.setVideoURI( Uri.parse(
+                        "android.resource://" + "com.bignerdranch.android.hellomoon/raw/apollo_17_stroll" ));
+
         return v;
     }
 
@@ -55,4 +75,6 @@ public class HelloMoonFragment  extends Fragment{
         super.onDestroy();
         mPlayer.stop();
     }
+
+
 }
